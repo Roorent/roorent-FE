d144a5356598e52f9a1503cc0cccb53b9f5bc26b
Danareee <muhdankah22@gmail.com>
2023-12-08 11:14:41 +0700

    fix: get api be Auth Login

--- app/(guest)/auth/login/page.tsx
+++ app/(guest)/auth/login/page.tsx
@@ -1,35 +1,60 @@
 "use client";
 
 import React, {useEffect, useState} from "react";
-// import {observer} from 'mobx-react-lite';
-import {Button, Card, Checkbox, Col, Form, Input, Row, Typography} from 'antd';
-import {LockOutlined, MailOutlined} from '@ant-design/icons';
+import {Button, Card, Checkbox, Col, Form, Input, Row, Typography, message} from 'antd/lib/index';
+import {EyeInvisibleOutlined, EyeTwoTone, LockOutlined, MailOutlined} from '@ant-design/icons';
 import {LOGIN} from "#/constants/images";
 import Logo from "#/components/logo/logo";
-// import ParticlesLayout from "../components/Layout/ParticlesLayout";
+import { useRouter } from "next/navigation";
+import { authRepository } from "#/repository/auth";
+
+interface ErrorLogin {
+    response : {
+        body : {
+            statusCode : number
+            error: string
+        }
+    }
+}
+
+interface SuccessLogin {
+    body: {
+        data: {
+            access_token : string
+        }
+        statusCode : number
+        message : string
+    }
+}
+
 
 const Login = () => {
     useEffect(() => {
 		document.title = "Login - Roorent";
 	}, []);
-    // const store = useStore();
-    const [loading, setLoading] = useState(false);
+    
+    const router = useRouter()
+    const [loading, setLoading] = useState(false)
 
-    // let history = useHistory();
-
-    const onFinish = (values: any) => {
+    const onFinish = async (values: any) => {
         console.log('Received values of form: ', values);
-        enterLoading(values).then(res => {
-            console.log(res, "awasaa");
-        }).catch((error) => {
-            console.log({error}, "awasaa error");
-        });
-    };
+        try {
+            const data = {
+                email: values?.email,
+                password: values?.password
+            }
 
-    const enterLoading = async (props: any) => {
-        // store.setInitialToken("ayayay", "clap");
-        // return history.push("/app/page_example_1");
-    };
+            const login = await authRepository.manipulatedata.login(data)
+            // const token = (login as SuccessLogin)?.body?.data 
+
+            localStorage.setItem("access_token", login?.body?.data?.access_token)
+            // setLoading(false)
+            router.push("/adm/dashboard")
+        } catch (err) {
+            // console.log(err.response.body?.error);
+            message.error(err.response.body?.error)
+        }
+    }
 
     return (
     <div className="w-full h-full">
@@ -37,22 +62,22 @@ const Login = () => {
         <div className="w-1/2 relative">
             <div className="px-28 py-48 w-[800px]">
                 <div className="w-full">
-                    <Form name="login" className="login">
+                <Form name="login" className="login" onFinish={onFinish}>
                     <div className="flex flex-col space-y-5 w-full">
                         <div className="text-white text-5xl font-bold"><p>Masuk</p></div>
                         <div className="text-white text-xl">
                             <p className="mb-2">Jika kamu belum memiliki akun</p> 
-                            <p>kamu bisa <a href="#" className="font-bold no-underline hover:underline">Daftar disini!</a></p>
+                            <p>kamu bisa <a href="/auth/register" className="font-bold no-underline hover:underline">Daftar disini!</a></p>
                         </div>
                         <div>
                             <p className="text-white text-3xl font-bold pb-3">Email</p>
                             <div className="w-full">
                             <Form.Item
                                 name="email"
-                                rules={[{ required: true, message: 'Harap Masukan Email Anda!' }]}
+                                rules={[{ required: true, message: 'Harap masukan email anda!' }]}
                             >
                                 <Input size="large" placeholder="Masukan Email" prefix={<MailOutlined 
-                                className="text-white text-3xl mr-5"/>} className=" p-[10px] bg-white bg-opacity-20 rounded-[10px] border border-white border-opacity-30 input-login text-xl" />
+                                className="text-white text-3xl mr-5"/>} className=" p-[10px] bg-white bg-opacity-20 rounded-[10px] border border-white border-opacity-30 login text-xl" />
                             </Form.Item>
                             </div>
                         </div>
@@ -61,10 +86,11 @@ const Login = () => {
                             <div className="w-full">
                             <Form.Item
                                 name="password"
-                                rules={[{ required: true, message: 'Harap Masukan Password Anda!' }]}
+                                rules={[{ required: true, message: 'Harap masukan password anda!' }]}
                             >
                                 <Input.Password size="large" placeholder="Masukan Password" prefix={<LockOutlined 
-                                className="text-white text-3xl mr-5"/>} className=" p-[10px] bg-white bg-opacity-20 rounded-[10px] border border-white border-opacity-30 input-login text-xl" />
+                                className="text-white text-3xl mr-5"/>} className=" p-[10px] bg-white bg-opacity-20 rounded-[10px] border border-white border-opacity-30 login text-xl"
+                                iconRender={(visible) => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}/>
                             </Form.Item>
                             </div>
                         </div>
@@ -74,12 +100,12 @@ const Login = () => {
                     </div>
                     <div className="w-full mt-10">
                     <Form.Item>
-                        <Button type="primary" htmlType="submit" block className="bg-tranparant border border-white rounded-full text-2xl font-bold py-3">
+                        <Button type="primary" loading={loading} htmlType="submit" block className="bg-tranparant border border-white rounded-full text-2xl font-bold py-3">
                             Masuk
                         </Button>
                     </Form.Item>
                     </div>
-                    </Form>
+                </Form>
                 </div>
             </div>
             <div className="w-[1258px] h-[1258px] bg-primary rounded-e-full -my-32 -ml-72 blur-[2px] absolute top-0 left-0  -z-50"></div>
@@ -94,117 +120,6 @@ const Login = () => {
         </div>
     </div>
     </div>)
-    // <div style={{width: '100vw', display: 'flex', justifyContent: 'center'}}>
-    //     <Row justify={'center'}>
-    //         <Col>
-    //             <div style={{
-    //                 display: 'flex',
-    //                 justifyContent: 'flex-start',
-    //                 marginTop: '5vh',
-    //                 flexDirection: 'column',
-    //                 alignItems: 'center',
-    //             }}>
-    //                 <div style={{display: 'flex', flexDirection: 'column', alignItems: 'stretch'}}>
-    //                     <Typography.Paragraph
-    //                         style={{
-    //                             margin: 0,
-    //                             padding: 0,
-    //                             fontSize: 20,
-    //                             marginLeft: 5,
-    //                             fontWeight: 600,
-    //                             color: "#413d3e",
-    //                         }}
-    //                     >
-    //                         Boilerplate
-    //                     </Typography.Paragraph>
-    //                 </div>
-    //                 <Card
-    //                     style={{width: 320, textAlign: 'center'}}
-    //                     headStyle={{fontSize: 13, fontWeight: 200}}
-    //                     className={"shadow"}
-    //                     bordered={true}
-    //                     title={'Sign in to your account'}
-    //                 >
-    //                     <Form
-    //                         layout={'vertical'}
-    //                         name="normal_login"
-    //                         className="login-form"
-    //                         onFinish={onFinish}
-    //                     >
-    //                         <Form.Item
-    //                             label="Email"
-    //                             name="email"
-    //                             // size={'large'}
-    //                             rules={[{required: false, message: 'Please input your Username!'}]}
-    //                         >
-    //                             <Input
-    //                                 prefix={<UserOutlined className="site-form-item-icon"/>}
-    //                                 type="text"
-    //                                 placeholder="Email"/>
-    //                         </Form.Item>
-
-    //                         <Form.Item
-    //                             style={{
-    //                                 marginBottom: 0,
-    //                             }}
-    //                             label="Password"
-    //                             name="password"
-    //                             // size={'large'}
-    //                             rules={[{required: false, message: 'Please input your Password!'}]}
-    //                         >
-    //                             <Input.Password
-    //                                 prefix={<LockOutlined className="site-form-item-icon"/>}
-    //                                 type="password"
-    //                                 placeholder="Password"
-    //                             />
-    //                         </Form.Item>
-    //                         <Form.Item
-    //                             style={{
-    //                                 marginTop: 0,
-    //                                 marginBottom: 20,
-    //                                 padding: 0
-    //                             }}
-    //                             // label="Password"
-    //                             name="forgot-password"
-    //                             // size={'small'}
-    //                             rules={[{required: false, message: 'Please input your Password!'}]}
-    //                         >
-    //                             <a className="login-form-forgot" href="">
-    //                                 Forgot password
-    //                             </a>
-    //                         </Form.Item>
-
-    //                         <Form.Item
-    //                             style={{
-    //                                 marginBottom: 5,
-    //                                 textAlign: 'left'
-    //                             }}>
-    //                             <Form.Item name="remember" valuePropName="checked" noStyle>
-    //                                 <Checkbox>Remember me</Checkbox>
-    //                             </Form.Item>
-    //                         </Form.Item>
-
-    //                         <Form.Item
-    //                             style={{
-    //                                 marginBottom: 0,
-    //                             }}>
-    //                             <Button type="primary"
-    //                                     block
-    //                                     loading={loading}
-    //                                     htmlType="submit"
-    //                                     size={'large'}
-    //                                     onSubmit={enterLoading}
-    //                                     className="login-form-button">
-    //                                 Sign In
-    //                             </Button>
-    //                         </Form.Item>
-    //                     </Form>
-    //                 </Card>
-    //             </div>
-    //         </Col>
-    //     </Row>
-
-    // </div>;
 };
 
 export default Login;
---
+++ repository/auth.ts
@@ -0,0 +1,16 @@
+import { http } from "#/utils/http"
+
+const url = {
+    login : () => '/auth/login'
+}
+
+const manipulatedata = {
+    login(data: any){
+        return http.post(url.login()).send(data)
+    }
+}
+
+export const authRepository = {
+    url,
+    manipulatedata
+}
